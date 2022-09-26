2858071ddd461244de5e0954ca0741c9561782cf
qnhanhh <caoquynhanh18@gmail.com>
2022-09-26 17:26:31 +0700

    sign in with pop up is much easier!!!

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,20 +1,9 @@
-import { useEffect } from "react";
-import { getRedirectResult } from "firebase/auth";
 import {
-  auth,
   signInWithGooglePopup,
-  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from "../../utils/firebase/firebase.utils";
 
 const SignIn = () => {
-  useEffect(async () => {
-    const res = await getRedirectResult(auth);
-    if (res) {
-      const userDocRef = await createUserDocumentFromAuth(res.user);
-    }
-  }, []);
-
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
@@ -24,9 +13,6 @@ const SignIn = () => {
     <div>
       <h1>Sign in page</h1>
       <button onClick={logGoogleUser}>Sign In with Google pop up</button>
-      <button onClick={signInWithGoogleRedirect}>
-        Sign In with Google redirect
-      </button>
     </div>
   );
 };
