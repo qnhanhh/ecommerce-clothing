d3eb765e17693f4cd11441ee71404b506c530bc9
qnhanhh <caoquynhanh18@gmail.com>
2022-09-27 11:05:25 +0700

    signin and signup with email and password

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,44 +1,100 @@
-import { useState } from "react"
+import { useState } from "react";
+import {
+  createAuthUserWithEmailAndPassword,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
 
-const defaultFormFields={
-    displayName:'',
-    email:'',
-    password:'',
-    confirmPassword:''
-}
+const defaultFormFields = {
+  displayName: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
 
-const SignUpForm=()=>{
-    const [formFields, setFormFields]=useState(defaultFormFields)
-    const {displayName, email, password, confirmPassword}=formFields
+const SignUpForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { displayName, email, password, confirmPassword } = formFields;
 
-    const handleChange=(e)=>{
-        const {name, value}=e.target
-        setFormFields({
-            ...formFields,
-            [name]:value
-        })
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormFields({
+      ...formFields,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      alert("passwords do not match");
+      return;
     }
+    try {
+      const { user } = await createAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+      await createUserDocumentFromAuth(user, { displayName });
+      resetFormFields();
+    } catch (error) {
+      if (error.code === "auth/email-already-in-use") {
+        alert("cannot create user, email already in use");
+      } else if (error.code === "auth/weak-password") {
+        alert("password should be at least 6 characters");
+      } else {
+        console.log(error.message);
+      }
+    }
+  };
 
-    return(
-        <div>
-            <h1>Sign up with your email and password</h1>
-            <form onSubmit={()=>{}}>
-                <label>Display Name</label>
-                <input value={displayName} type='text' required onChange={handleChange} name='displayName'/>
+  return (
+    <div>
+      <h1>Sign up with your email and password</h1>
+      <form onSubmit={handleSubmit}>
+        <label>Display Name</label>
+        <input
+          value={displayName}
+          type="text"
+          required
+          onChange={handleChange}
+          name="displayName"
+        />
 
-                <label>Email</label>
-                <input value={email} type='email' required onChange={handleChange} name='email'/>
+        <label>Email</label>
+        <input
+          value={email}
+          type="email"
+          required
+          onChange={handleChange}
+          name="email"
+        />
 
-                <label>Password</label>
-                <input value={password} type='password' required onChange={handleChange} name='password'/>
+        <label>Password</label>
+        <input
+          value={password}
+          type="password"
+          required
+          onChange={handleChange}
+          name="password"
+        />
 
-                <label>Confirm Password</label>
-                <input value={confirmPassword} type='password' required onChange={handleChange} name='confirmPassword'/>
+        <label>Confirm Password</label>
+        <input
+          value={confirmPassword}
+          type="password"
+          required
+          onChange={handleChange}
+          name="confirmPassword"
+        />
 
-                <button type='submit'>Sign Up</button>
-            </form>
-        </div>
-    )
-}
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+};
 
-export default SignUpForm
+export default SignUpForm;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app'
-import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth'
+import { getAuth, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword } from 'firebase/auth'
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore'
 
 const firebaseConfig = {
@@ -23,7 +23,8 @@ export const signInWithGooglePopup = () => signInWithPopup(auth, provider)
 
 export const db = getFirestore()
 
-export const createUserDocumentFromAuth = async (userAuth) => {
+export const createUserDocumentFromAuth = async (userAuth, additionalInformation={}) => {
+    if (!userAuth) return
     const userDocRef = doc(db, 'users', userAuth.uid)
 
     const userSnapshot = await getDoc(userDocRef)
@@ -37,7 +38,8 @@ export const createUserDocumentFromAuth = async (userAuth) => {
             await setDoc(userDocRef, {
                 displayName,
                 email,
-                createdAt
+                createdAt,
+                ...additionalInformation,
             })
         } catch (error) {
             console.log(error.message)
@@ -47,4 +49,9 @@ export const createUserDocumentFromAuth = async (userAuth) => {
     //if user data exists
     //return userDocRef
     return userDocRef
+}
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+    if (!email || !password) return
+    return await createUserWithEmailAndPassword(auth, email, password)
 }
