ce4f40ff61d587e75b94d331f22f41b321a0c3c2
qnhanhh <caoquynhanh18@gmail.com>
2022-09-28 11:29:56 +0700

    add cart dropdown ui

--- src/App.js
+++ src/App.js
@@ -2,14 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Navigation from './routes/navigation/navigation.component'
 import Home from "./routes/home/home.component"
 import Authentication from './routes/authentication/authentication.component'
-
-const Shop=()=>{
-  return(
-    <div>
-      <h1>Im shop page</h1>
-    </div>
-  )
-}
+import Shop from './routes/shop/shop.component'
 
 const App = () => {
   return (
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,6 +1,8 @@
 import { Fragment, useContext } from "react";
 import { Link, Outlet } from "react-router-dom";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import "./navigation.styles.scss";
@@ -19,13 +21,17 @@ const Navigation = () => {
             SHOP
           </Link>
           {currentUser ? (
-            <span className="nav-link" onClick={signOutUser}>SIGN OUT</span>
+            <span className="nav-link" onClick={signOutUser}>
+              SIGN OUT
+            </span>
           ) : (
             <Link className="nav-link" to="/auth">
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
+        <CartDropdown />
       </div>
       <Outlet />
     </Fragment>
