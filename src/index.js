8e6b9b7210d4d9a911797c2b63b1cbbeea3f78b2
qnhanhh <caoquynhanh18@gmail.com>
2022-09-28 11:50:43 +0700

    toggle cart dropdown

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { UserProvider } from './contexts/user.context';
 import { ProductsProvider } from './contexts/products.context';
+import { CartProvider } from './contexts/cart.context';
 import App from './App';
 import './index.scss';
 
@@ -13,7 +14,9 @@ root.render(
     <BrowserRouter>
       <UserProvider>
         <ProductsProvider>
-          <App />
+          <CartProvider>
+            <App />
+          </CartProvider>
         </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
