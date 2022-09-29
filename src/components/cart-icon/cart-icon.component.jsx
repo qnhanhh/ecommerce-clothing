0b27d3764f9108fb29678edb974b2e080604b018
qnhanhh <caoquynhanh18@gmail.com>
2022-09-29 12:05:10 +0700

    cart count

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -4,12 +4,12 @@ import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg'
 import './cart-icon.styles.scss'
 
 const CartIcon=()=>{
-    const {isCartOpen, setIsCartOpen}=useContext(CartContext)
+    const {isCartOpen, setIsCartOpen, cartCount}=useContext(CartContext)
 
     return(
         <div className='cart-icon-container' onClick={()=>setIsCartOpen(!isCartOpen)}>
             <ShoppingIcon className='shopping-icon' />
-            <span className='item-count'>10</span>
+            <span className='item-count'>{cartCount}</span>
         </div>
     )
 }
