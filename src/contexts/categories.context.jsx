1a6df2df78635fd4e6dca087d50a61c5911982d6
qnhanhh <caoquynhanh18@gmail.com>
2022-10-05 15:43:56 +0700

    config categories reducer

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -1,6 +1,5 @@
 import { createContext, useEffect, useState } from "react";
 import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
-import SHOP_DATA from "../shop-data.js";
 
 export const CategoriesContext = createContext({
   categoriesMap: {},
--- src/store/user/user.reducer.js
+++ src/store/user/user.reducer.js
@@ -1,12 +1,12 @@
 import { USER_ACTION_TYPES } from "./user.types";
 
-const INITIAL_STATE = {
+const USER_INITIAL_STATE = {
     currentUser: null
 }
 
 //in redux, every single reducer receives every single action
 //=> by default, return current state => the code knows that this part of the reducer didn't change
-export const userReducer = (state = INITIAL_STATE, action) => {
+export const userReducer = (state = USER_INITIAL_STATE, action) => {
     const { type, payload } = action
 
     switch (type) {
