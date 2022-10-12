941ca7fb0e51544d68ff8402148f92969dd0979b
qnhanhh <caoquynhanh18@gmail.com>
2022-10-12 10:55:59 +0700

    setting up stripe for payment

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
+import { Elements } from '@stripe/react-stripe-js'
 
 import reportWebVitals from './reportWebVitals';
-import { store , persistor} from './store/store'
+import { store, persistor } from './store/store'
 import App from './App';
+import { stripePromise } from './utils/stripe/stripe.utils'
+
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -15,7 +18,9 @@ root.render(
     <Provider store={store}>
       <PersistGate loading persistor={persistor}>
         <BrowserRouter>
-          <App />
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
         </BrowserRouter>
       </PersistGate>
     </Provider>
