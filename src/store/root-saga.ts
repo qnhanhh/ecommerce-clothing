a2eca8a20a739fe239a17a617f92ce4e10c4b60b
qnhanhh <caoquynhanh18@gmail.com>
2022-10-17 14:17:49 +0700

    typing root saga

--- src/store/root-saga.ts
+++ src/store/root-saga.ts
@@ -1,11 +1,11 @@
 //encapsulate all different sagas
 
-import {all, call} from 'redux-saga/effects'
+import {all, call} from 'typed-redux-saga/macro'
 
 import { categoriesSaga } from './categories/categories.saga'
 import { userSagas } from './user/user.saga'
 
 //generator function
 export function* rootSaga(){
-    yield all([call(categoriesSaga), call(userSagas)])
+    yield* all([call(categoriesSaga), call(userSagas)])
 }
