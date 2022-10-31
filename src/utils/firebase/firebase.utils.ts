677f813466875e6e0515141007d897f79bd4a1f0
qnhanhh <caoquynhanh18@gmail.com>
2022-10-31 11:51:17 +0700

    add ga

--- src/utils/firebase/firebase.utils.ts
+++ src/utils/firebase/firebase.utils.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app'
+import {getAnalytics} from 'firebase/analytics'
 import {
     getAuth,
     signInWithPopup,
@@ -32,7 +33,8 @@ const firebaseConfig = {
     appId: "1:130866350963:web:1682de82974babc8fa9306"
 }
 
-initializeApp(firebaseConfig)
+const app=initializeApp(firebaseConfig)
+const analytics=getAnalytics(app)
 
 const provider = new GoogleAuthProvider()
 provider.setCustomParameters({
