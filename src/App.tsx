c4716deb2e1856372c06573d4a671ce918a47882
qnhanhh <caoquynhanh18@gmail.com>
2022-11-16 13:56:49 +0700

    add to homescreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,66 @@
-import { useEffect } from 'react'
-import { Routes, Route } from 'react-router-dom'
-import { useSetRecoilState } from 'recoil'
+import { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import { useSetRecoilState } from "recoil";
 
-import { userState } from './recoil/user/user.state'
-import Navigation from './routes/navigation/navigation.component'
-import Home from "./routes/home/home.component"
-import Authentication from './routes/authentication/authentication.component'
-import Shop from './routes/shop/shop.component'
-import Checkout from './routes/checkout/checkout.component'
-import { isUserAuthenticated } from './recoil/user/user.actions'
-import { UserData } from './utils/firebase/firebase.utils'
+import { userState } from "./recoil/user/user.state";
+import Navigation from "./routes/navigation/navigation.component";
+import Home from "./routes/home/home.component";
+import Authentication from "./routes/authentication/authentication.component";
+import Shop from "./routes/shop/shop.component";
+import Checkout from "./routes/checkout/checkout.component";
+import { isUserAuthenticated } from "./recoil/user/user.actions";
+import { UserData } from "./utils/firebase/firebase.utils";
 
 const App = () => {
-  const setState = useSetRecoilState(userState)
+  const setState = useSetRecoilState(userState);
 
   useEffect(() => {
     const checkUserSession = async () => {
       try {
-        const userAuth = await isUserAuthenticated()
-        setState(prevState => {
-          return { ...prevState, currentUser: userAuth as UserData }
-        })
+        const userAuth = await isUserAuthenticated();
+        setState((prevState) => {
+          return { ...prevState, currentUser: userAuth as UserData };
+        });
       } catch (error) {
-        alert(error)
+        alert(error);
       }
-    }
-    checkUserSession()
+    };
+    checkUserSession();
   }, [setState]);
 
+  let promptEvent: BeforeInstallPromptEvent;
+
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", (e) => {
+      e.preventDefault()
+      promptEvent = e as BeforeInstallPromptEvent;
+    });
+  }, []);
+
+  const handleClick = () => {
+    promptEvent.prompt()
+    promptEvent.userChoice.then(res=>{
+      if(res.outcome==='accepted'){
+        console.log('a2hs');
+      }else{
+        console.log('no a2hs');
+      }
+    })
+  };
+
   return (
-    <Routes>
-      <Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path='shop/*' element={<Shop />} />
-        <Route path='auth' element={<Authentication />} />
-        <Route path='checkout' element={<Checkout />} />
-      </Route>
-    </Routes>
-  )
-}
+    <>
+      <h1 onClick={handleClick}>Add to screen</h1>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="auth" element={<Authentication />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </>
+  );
+};
 
 export default App;
--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,6 +1,21 @@
-declare module '*.svg' {
-    import React = require('react');
-    export const ReactComponent: React.FC<React.SVGProps<SVGElement>>;
-    const src: string;
-    export default src;
+// declare module '*.svg' {
+//     import React = require('react');
+//     export const ReactComponent: React.FC<React.SVGProps<SVGElement>>;
+//     const src: string;
+//     export default src;
+// }
+
+interface BeforeInstallPromptEvent extends Event {
+    readonly platforms: string[];
+    readonly userChoice: Promise<{
+        outcome: "accepted" | "dismissed";
+        platform: string;
+    }>;
+    prompt(): Promise<void>;
+}
+
+declare global {
+    interface WindowEventMap {
+        beforeinstallprompt: BeforeInstallPromptEvent;
+    }
 }
